0cc4f404bae193e4d567b72fdddfd9bc4eb3132d
unknown <Hyourinmaru80@gmail.com>
2018-11-07 15:11:12 +0200

    final commit

---
+++ build/precache-manifest.9982575a1d726bdb9e78c7ed002bbe5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8203c05fc6eb37a0743f",
+    "url": "/static/js/main.8203c05f.chunk.js"
+  },
+  {
+    "revision": "12826cfd7b9e28c3e1c7",
+    "url": "/static/js/1.12826cfd.chunk.js"
+  },
+  {
+    "revision": "8203c05fc6eb37a0743f",
+    "url": "/static/css/main.85528177.chunk.css"
+  },
+  {
+    "revision": "3bcd6ecd31662a5d25ab8a6c5d9ecd6c",
+    "url": "/index.html"
+  }
+];
