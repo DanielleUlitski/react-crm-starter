1c6e20b2d18f1ec32cb32938ec9ec6ce8bc1449e
unknown <Hyourinmaru80@gmail.com>
2018-11-07 14:21:03 +0200

    production-ready

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const path = require('path')
 const server = require('http').Server(app);
 // const io = require('socket.io').listen(server);
 const router = require('./routes/router')
-server.listen(8090);
+server.listen(process.env.PORT || '8090');
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
 
-    next()
-})
-app.use(express.static(path.join(__dirname, 'client')))
+//     next()
+// })
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client')))
 app.use(router);
 
 // io.sockets.on('connection', (socket) => {
